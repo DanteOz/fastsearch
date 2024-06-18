e56576d3c195edf044de59d7fd60ab39560103c7
Dante Oz <me@danteoz.com>
2024-06-18 14:37:39 -0400

    Add valibot validation

--- frontend/src/lib/Context.tsx
+++ frontend/src/lib/Context.tsx
@@ -9,31 +9,35 @@ import {
   createEffect,
 } from "solid-js";
 import { createStore, SetStoreFunction } from "solid-js/store";
+import * as v from "valibot";
 
-export interface SearchResult {
-  id: string;
-  video_id: string;
-  title: string;
-  text: string;
-  start: number;
-  thumbnail: string;
-  lesson: string | null;
-  forum: string | null;
-  course: string | null;
-}
+const ResultsSchema = v.array(
+  v.object({
+    id: v.string(),
+    video_id: v.string(),
+    title: v.string(),
+    text: v.string(),
+    start: v.number(),
+    thumbnail: v.string(),
+    lesson: v.nullable(v.string()),
+    forum: v.nullable(v.string()),
+    course: v.nullable(v.string()),
+  })
+);
 
-const fetchResults = async (query: string) => {
-  return await fetch("/api/search", {
+async function fetchResults(query: string) {
+  const resp = await fetch("/api/search", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ query: query }),
-  }).then((response) => {
-    if (response.ok) {
-      return response.json() as Promise<SearchResult[]>;
-    }
-    throw new Error(`${response.status} | ${response.statusText}`);
   });
-};
+  if (!resp.ok) {
+    throw new Error(`${resp.status} | ${resp.statusText}`);
+  }
+  const data = await resp.json();
+  const results = v.parse(ResultsSchema, data);
+  return results;
+}
 
 export type Feedback = -1 | null | 1;
 export const initFeedback = (n: number) => Array(n).fill(null);
@@ -41,7 +45,7 @@ export const initFeedback = (n: number) => Array(n).fill(null);
 interface ContextProps {
   query: Accessor<string | null>;
   setQuery: Setter<string | null>;
-  results: InitializedResource<SearchResult[]>;
+  results: InitializedResource<v.InferOutput<typeof ResultsSchema>>;
   feedback: Feedback[];
   setFeedback: SetStoreFunction<Feedback[]>;
   selected: Accessor<number | null>;
