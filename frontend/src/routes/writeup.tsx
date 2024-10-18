00d5a1f70e0a713567c80e5e928cabfc6cba9c30
Dante Oz <me@danteoz.com>
2024-10-18 01:47:37 -0400

    update favicon

--- frontend/src/routes/writeup.tsx
+++ frontend/src/routes/writeup.tsx
@@ -1,3 +1,4 @@
+import { Link } from "@solidjs/meta";
 import type { RouteDefinition } from "@solidjs/router";
 import { WriteupNav } from "~/components/writeup/Nav";
 import "~/styles/Writeup.css";
@@ -5,6 +6,13 @@ import "~/styles/Writeup.css";
 export default function Layout(props: RouteDefinition) {
   return (
     <>
+      <Link rel="icon" type="image/x-icon" href="/writeup/favicon.ico" />
+      <Link rel="icon" type="image/x-icns" href="/writeup/favicon.icns" />
+      <Link rel="icon" type="image/png" href="/writeup/favicon_16x16.png" sizes="16x16" />
+      <Link rel="icon" type="image/png" href="/writeup/favicon_32x32.png" sizes="32x32" />
+      <Link rel="icon" type="image/png" href="/writeup/favicon_128x128.png" sizes="128x128" />
+      <Link rel="icon" type="image/png" href="/writeup/favicon_256x256.png" sizes="256x256" />
+      <Link rel="icon" type="image/png" href="/writeup/favicon_512x512.png" sizes="512x512" />
       <WriteupNav />
       {props.children}
     </>
