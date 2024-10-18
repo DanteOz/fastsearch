3a9b3ef03931cf17decb5fb7e0ffaaf26a745757
Dante Oz <me@danteoz.com>
2024-10-18 02:28:48 -0400

    update favicon locations

--- frontend/src/routes/writeup.tsx
+++ frontend/src/routes/writeup.tsx
@@ -6,13 +6,13 @@ import "~/styles/Writeup.css";
 export default function Layout(props: RouteDefinition) {
   return (
     <>
-      <Link rel="icon" type="image/x-icon" href="/writeup/favicon.ico" />
-      <Link rel="icon" type="image/x-icns" href="/writeup/favicon.icns" />
-      <Link rel="icon" type="image/png" href="/writeup/favicon_16x16.png" sizes="16x16" />
-      <Link rel="icon" type="image/png" href="/writeup/favicon_32x32.png" sizes="32x32" />
-      <Link rel="icon" type="image/png" href="/writeup/favicon_128x128.png" sizes="128x128" />
-      <Link rel="icon" type="image/png" href="/writeup/favicon_256x256.png" sizes="256x256" />
-      <Link rel="icon" type="image/png" href="/writeup/favicon_512x512.png" sizes="512x512" />
+      <Link rel="icon" type="image/x-icon" href="/img/writeup/favicon.ico" />
+      <Link rel="icon" type="image/x-icns" href="/img/writeup/favicon.icns" />
+      <Link rel="icon" type="image/png" href="/img/writeup/favicon_16x16.png" sizes="16x16" />
+      <Link rel="icon" type="image/png" href="/img/writeup/favicon_32x32.png" sizes="32x32" />
+      <Link rel="icon" type="image/png" href="/img/writeup/favicon_128x128.png" sizes="128x128" />
+      <Link rel="icon" type="image/png" href="/img/writeup/favicon_256x256.png" sizes="256x256" />
+      <Link rel="icon" type="image/png" href="/img/writeup/favicon_512x512.png" sizes="512x512" />
       <WriteupNav />
       {props.children}
     </>
