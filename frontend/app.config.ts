3ae22692ff18d211c177247295a4933eb0bc2704
Dante Oz <me@danteoz.com>
2025-05-11 16:03:42 -0400

    chore: bump deps
    - bump Node version
    - add compt date
    - bump frontend deps
    - disable ping action

--- frontend/app.config.ts
+++ frontend/app.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   ssr: false,
   vite: {
     server: {
+      compatibilityDate: "2025-05-11",
       proxy: {
         "/api/": {
           target: "http://localhost:8000/",
